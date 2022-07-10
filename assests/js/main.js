f9633a00ffdab3c6790945b38dfcaf0c60785413
Kamran Hasanov <kamranhasanov128@gmail.com>
2022-07-10 20:34:10 +0400

    lesson18 ev tapsirigi

---
+++ assests/js/main.js
@@ -0,0 +1,52 @@
+
+
+
+////////1. birinci tapsiriq: istifadeciden reqem al ve onlari topla vur cix vs....
+
+// var number1 = Number(prompt("1 ci ədədi göstərin:"));
+// var number2 = Number(prompt("2 ci ədədi göstərin:"));
+
+// cem = number1 + number2;
+// cixma = number1 - number2;
+// vurma = number1 * number2;
+// bolme = number1 / number2;
+// kokalma = number1 % number2;
+// kvatrati = number1 ** number2;
+
+
+// console.log(cem, cixma, vurma, bolme, kokalma, kvatrati);
+
+//////////////////////////////// 1-1 ci tapsiriq : 1)	Ekranda “salam dunya” yazısını göstərin
+// var hello = alert("Salam Dunya");
+
+
+//////////////////5)İstifadəçidən onun adını, soyadını və yaşını soruşun. Alınan məlumatları ekranda “Mənim adım xxxx, soyadım xxxx, yaşım isə xxx-dir” şəklində göstər
+
+// let firtName = prompt("Adınız");
+// let lastName = prompt("Soyadınız");
+// let age = Number(prompt("Yaşınız:"));
+
+// console.log("Mənim Adım: " + firtName);
+// console.log("Mənim Soyadım: " + lastName);
+// console.log("Mənim Yaşım: " + age);
+
+//////////////////////////////6)	Ədədin kubunu göstərən proqram yazın.
+
+// let number = Number(prompt("Nəyin Kubu?"));
+// let xcube = 3;
+
+// cube = number ** xcube;
+// console.log(number + " Ədədinin Kvadratı budur: " + cube);
+
+
+//////////////////////////////7)Düzbucaqlı fiqurumuz var. İstifadəçidən düzbucaqlının eni və uzunluğunu alaraq onun perimetr və sahəsini hesablayan proqram yazın
+
+let a = Number(prompt("Duzbucaqlının a Neçədir?"));
+let b = Number(prompt("Duzbucaqlının b Neçədir?"));
+let c = Number(prompt("Duzbucaqlının c Neçədir?"));
+
+P = a+b+c;
+S = a * b / 2;
+
+console.log("Düzbucaqlının Perimetri: "+ P);
+console.log("Düzbucaqlının Sahəsi: "+ S);
